f84ef09747d6fed890a573693b4026c094cbff2f
unknown <>
2017-03-08 20:49:53 +0800

    项目开张~

---
+++ src/main/java/com/acfun/AcerApplication.java
@@ -0,0 +1,12 @@
+package com.acfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcerApplication.class, args);
+	}
+}
