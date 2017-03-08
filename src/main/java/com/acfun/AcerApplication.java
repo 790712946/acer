83f5135834afe500c92f8a8971d3dc98fee431d4
jianghong <790712946@qq.com>
2017-03-08 21:52:17 +0800

    Hello,World,配置Nginx

--- src/main/java/com/acfun/AcerApplication.java
+++ src/main/java/com/acfun/AcerApplication.java
@@ -2,11 +2,18 @@ package com.acfun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AcerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcerApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String test(){
+		return "Hello,World";
 	}
 }
