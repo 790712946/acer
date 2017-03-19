345c1afb79123e56af64986c937d3213ff8f0145
jianghong <790712946@qq.com>
2017-03-19 21:40:46 +0800

    更新下gitignore文件

--- src/main/java/com/acfun/AcerApplication.java
+++ src/main/java/com/acfun/AcerApplication.java
@@ -6,17 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 @RestController
 public class AcerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcerApplication.class, args);
-		List<Person> list=new ArrayList<>();
-		list.forEach(person -> System.out.println(person.getFirstName()));
 	}
 	@RequestMapping("/")
 	public String test(){
